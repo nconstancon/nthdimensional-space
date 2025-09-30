f9cb82ebc9d6b9bff1387e2fe7d101f745fc311d
Nikki Constancon <nikki.constancon@epiuse.com>
2025-09-30 12:47:15 +0200

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
     <main style={{
-      minHeight: '100vh',
+      height: '100vh',
+      boxSizing: 'border-box',
+      overflow: 'hidden',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
